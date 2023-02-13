d3ef1a26f55d63f9229f26c798e1039054f94124
Subir Gupta <85447146+rubisq@users.noreply.github.com>
2023-02-13 20:05:06 +0530

    Initial Commit

---
+++ AssignmentShyamFutureTechLLP/AssignmentShyamFutureTechLLPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentShyamFutureTechLLPApp.swift
+//  AssignmentShyamFutureTechLLP
+//
+//  Created by openweb on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentShyamFutureTechLLPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
